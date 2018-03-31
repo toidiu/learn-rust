e4f94ff9339462ab191389561d6ea140ea33f698
toidiu <toidiu@protonmail.com>
2018-03-31 17:14:15 -0400

    update to simpler reqwest and a compiler error

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
-#![deny(warnings)]
-extern crate futures;
-extern crate hyper;
-extern crate pretty_env_logger;
-extern crate quick_xml;
-
-use std::io::Read;
-use hyper::Client;
+extern crate reqwest;
 
 fn main() {
-    println!("=================================");
-    let mut xml_resp = String::new();
-    get_mta_status(&mut xml_resp);
+    get_mta_status();
 }
 
-fn get_mta_status(xml: &mut String) {
-    let client = Client::new();
-    client
-        .get("http://web.mta.info/status/serviceStatus.txt")
-        .send()
-        .unwrap()
-        .read_to_string(xml)
-        .unwrap();
-    println!("{}", xml);
+fn get_mta_status() {
+    let body = reqwest::get("http://web.mta.info/status/serviceStatus.txt").unwrap()
+            .text().unwrap();
+
+    println!("{:?}", body);
+    body
 }
