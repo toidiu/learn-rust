b1acaf97f38c71466e7f5617f8f5c6fea0be4a9d
toidiu <toidiu@protonmail.com>
2018-03-31 18:43:35 -0400

    ownership intro

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,15 @@ use reqwest::Response;
 
 fn main() {
     let client = Client::new();
-    let xml: String = get_mta_status();
+
+    let xml: String = get_mta_status(client);
+
+    let request2: String = get_mta_status(client);
 }
 
+// hint: pass a reference instead of moving ownership
 fn get_mta_status(client: Client) -> String {
-    let resp: Response = client
+    let mut resp: Response = client
         .get("http://web.mta.info/status/serviceStatus.txt")
         .send().unwrap();
     let body: String = resp.text().unwrap();
