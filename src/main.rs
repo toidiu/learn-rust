a7b0a6a5b9216273f8016def29fa5f804cdd7b9b
toidiu <toidiu@protonmail.com>
2018-03-31 18:24:44 -0400

    parse xml intelligently, look up how to convert from String to &str and implement enum variant.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![deny(warnings)]
+
 extern crate reqwest;
 extern crate xml;
 
@@ -8,7 +10,7 @@ fn main() {
 
     // passing `String` wont work because String doesnt implement std::io::Read
     // parse_xml(xml);
-    let readable: &[u8] = xml;
+    let readable: &[u8] = xml.as_bytes();
     parse_xml(readable);
 }
 
@@ -22,17 +24,63 @@ fn get_mta_status() -> String {
     body
 }
 
-fn parse_xml<T>(readable: T) where T: std::io::Read {
+#[derive(PartialEq)]
+enum XmlTag {
+    TimeStamp,
+    LineName,
+    LineStatus,
+    Ignore,
+}
+
+fn parse_xml<T>(readable: T)
+where
+    T: std::io::Read,
+{
     let reader = EventReader::new(readable);
+    let mut xml_tag: XmlTag = XmlTag::Ignore;
 
     for e in reader {
         match e {
-            Ok(XmlEvent::StartElement { name, .. }) => print!("start: {}", name),
-            Ok(XmlEvent::EndElement { name }) => println!("    end: {}", name),
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                let ref_name: &str = name.local_name;
+                match ref_name {
+                    "timestamp" => {
+                        xml_tag = XmlTag::TimeStamp;
+                        print!("{}: ", name);
+                    }
+
+                    "name" => {
+                        xml_tag = XmlTag::LineName;
+                        print!("{}: ", name);
+                    }
+
+                    _ => {
+                        xml_tag = XmlTag::Ignore;
+                    }
+                }
+            }
+
+            Ok(XmlEvent::Characters(name)) => match xml_tag {
+                XmlTag::TimeStamp => println!("{}", name),
+                XmlTag::LineName => println!("{}", name),
+                _ => (),
+            },
+
+            Ok(XmlEvent::EndElement { name }) => {
+                let ref_name: &str = name.local_name;
+                match ref_name {
+                    // we only care about subway
+                    "subway" => break,
+
+                    _ => (),
+                }
+            }
+
             Err(e) => {
                 println!("Error: {}", e);
                 break;
             }
+
             _ => (),
         }
     }
